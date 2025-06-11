42dfd2b6e6359189fb8eb898657678891d3626e8
likepeng01 <likepeng01@baidu.com>
2025-06-11 13:21:14 +0800

    disable multi-viewport

--- launcher/launcher.cpp
+++ launcher/launcher.cpp
@@ -113,7 +113,7 @@ int main(int argc, char **argv) {
   io.ConfigFlags |=
       ImGuiConfigFlags_NavEnableGamepad;             // Enable Gamepad Controls
   io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;  // Enable Docking
-  io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;  // Enable Multi-Viewport
+  // io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;  // Enable Multi-Viewport
                                                        // / Platform Windows
   // io.ConfigViewportsNoAutoMerge = true;
   // io.ConfigViewportsNoTaskBarIcon = true;
